87abcff255e9e3c28123a114e619b81f8a43ac4d
hamzaabdous <abdoushamza6@gmail.com>
2022-03-14 21:25:19 +0100

    main on progress

--- src/View/Component/Main.js
+++ src/View/Component/Main.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../style/Main.css";
+import img1 from "../../Assets/Images/Sun cloud mid rain.png";
 const Main = () => {
   return (
     <div className="d-flex justify-content-center align-items-center ">
-      <div className="glass d-flex justify-content-center  ">
-      c
+      <div className="glass">
+        <div class=" d-flex bd-highlight">
+          <div class="p-2  bd-highlight">
+            <img src={img1} alt="" width={130} height={130} />
+          </div>
+          <div class="m-4 p-2 bd-highlight fs-3">
+            RABAT
+            <br />
+            20*C
+          </div>
+        </div>
+        <div>c</div>
       </div>
     </div>
   );
--- src/View/Weather.js
+++ src/View/Weather.js
@@ -4,19 +4,39 @@ import Footer from "./Component/Footer.js";
 import Nav from "./Component/Nav.js";
 import Main from "./Component/Main.js";
 const Weather = () => {
+  function name() {
+    console.log("vvv");
+  }
   return (
     <>
-      <div className="container-fluid" style={{ backgroundColor: "#F18989" }}>
+      <div className="container-fluid" style={{ backgroundColor: "#7dd3fc" }}>
         <header className="row">
           <Nav />
           <div className="row d-flex justify-content-center align-items-center">
             <input
-              style={{ width: "30em", height: "", borderRadius: "10px" }}
+              style={{
+                width: "30em",
+                height: "2em",
+                borderRadius: "9px",
+                border: " solid 1px #fff",
+              }}
               type="text"
               name="text"
               id=""
               placeholder="City"
             />
+
+            <lord-icon
+              onClick={name}
+              src="https://cdn.lordicon.com/msoeawqm.json"
+              on
+              trigger="click"
+              colors="primary:#121331,secondary:#fff"
+              style={{
+                width: "60px",
+                height: "50px",
+              }}
+            ></lord-icon>
           </div>
         </header>
         <main className="row m-5">
