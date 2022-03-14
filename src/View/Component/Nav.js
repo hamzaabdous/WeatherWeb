985ccb9f8b94e6cc612f11ab278f1986c0893108
hamzaabdous <abdoushamza6@gmail.com>
2022-03-14 16:43:37 +0100

    install bootstrap / create components

---
+++ src/View/Component/Nav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <div>
+      <nav className="row">
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1"></div>
+        <div className="col-sm-1">c</div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
--- src/View/Weather.js
+++ src/View/Weather.js
@@ -1,9 +1,25 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Footer from "./Component/Footer.js";
+import Nav from "./Component/Nav.js";
 
 const Weather = () => {
   return (
     <>
-      <div>hello</div>
+      <div className="container-fluid" style={{ backgroundColor: "#F18989" }}>
+        <header className="row">
+          <Nav />
+          <div className="row">
+            <input type="text" name="" id="" />
+          </div>
+        </header>
+        <main className="row d-flex justify-content-center align-items-center">
+          c
+        </main>
+        <footer className="row">
+          <Footer />
+        </footer>
+      </div>
     </>
   );
 };
