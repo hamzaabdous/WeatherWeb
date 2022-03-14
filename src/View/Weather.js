c2ffc30ae5694bafff6d52980732f72f53be2a57
hamzaabdous <abdoushamza6@gmail.com>
2022-03-14 16:53:05 +0100

    input City

--- src/View/Weather.js
+++ src/View/Weather.js
@@ -2,19 +2,25 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Footer from "./Component/Footer.js";
 import Nav from "./Component/Nav.js";
-
+import Main from "./Component/Main.js";
 const Weather = () => {
   return (
     <>
       <div className="container-fluid" style={{ backgroundColor: "#F18989" }}>
         <header className="row">
           <Nav />
-          <div className="row">
-            <input type="text" name="" id="" />
+          <div className="row d-flex justify-content-center align-items-center">
+            <input
+              style={{ width: "30em", height: "", borderRadius: "10px" }}
+              type="text"
+              name="text"
+              id=""
+              placeholder="City"
+            />
           </div>
         </header>
         <main className="row d-flex justify-content-center align-items-center">
-          c
+          <Main />
         </main>
         <footer className="row">
           <Footer />
